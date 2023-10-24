408d087a07a2d05ef5186c449411e3d924b76815
Shaun Tabone <shauntab@gmail.com>
2023-10-24 21:08:45 +0200

    fix: improved documentation

--- README.md
+++ README.md
@@ -1,70 +1,116 @@
-# Ubuntu Setup
+# Ubuntu Development Machine Setup
+
+<a href="screenshots/desktop.png"><p align="center"><img src="screenshots/desktop.png" /></p></a>
+*<p align="center">"Setup your Ubuntu dev machine, the way I like it! 😊"</p>*
+
+## Features
+
+* Updates system to latest version (DEB and Snap).
+* Installs essential basic tools such as `htop`, `curl`, `git` and `neovim`.
+* Adds Quake terminal.  This can be activated using `CTRL + F12`.
+* Adds command aliases:
+    * `..` equivalent to `cd ..`
+    * `...` equivalent to `cd ...`
+    * `....` equivalent to `cd ....`
+    * `.....` equivalent to `cd .....`
+    * `g` equivalent to `git`
+    * `ll` equivalent to `ls -l`
+* Adds git aliases:
+    * `g co` equivalent to `git checkout`
+    * `g ct` equivalent to `git commit`
+    * `g st` equivalent to `git status`
+    * `g br` equivalent to `git branch`
+    * `g p` equivalent to `git push`
+* Installs apps:
+    * [Brew](https://brew.sh/)
+    * [Docker](https://www.docker.com/)
+    * [.NET 7 + 8](https://dotnet.microsoft.com/)
+    * [Gitg](https://github.com/GNOME/gitg)
+    * [Google Chrome](https://www.google.com/chrome/)
+    * [Oh My Zsh](https://ohmyz.sh/) including plugins
+    * [Rider](https://www.jetbrains.com/rider/)
+    * [Tilix](https://gnunn1.github.io/tilix-web/)
+    * [Visual Studio Code](https://code.visualstudio.com/)
+* Installs fonts:
+    * [Nerd Font](https://www.nerdfonts.com/)
+    * [SF UI Font](https://developer.apple.com/fonts/)
 
 ## Getting Started
 
-1. Clone this repository
-    ```sh
-    git clone https://github.com/xontab/ubuntu-dev-machine.git
-    cd ubuntu-dev-machine
-    ```
+1. Open link to [Download ZIP](https://github.com/xontab/ubuntu-dev-machine/archive/refs/heads/main.zip).
+
+1. Extract ZIP file.
+
+1. Open extracted `ubuntu-dev-machine-main` folder.
+
+1. Right click and `Open in Terminal`.
 
 ## Development Setup
 
-1. Open `Terminal` application.
-
-1. Run the following commands:
+1. Run the following command:
     ```sh
-    bash scripts/dev.sh
+    bash install.run
     ```
+1. Select the items that you wish to install.  Use `Space` button to tick/untick the options. Then use `Tab` and `Enter` to confirm selection.
+
+1. In the next screen, confirm that the variables are correct.  Press `Enter` to proceed.
+
+1. After the installation is completed, click on the `Enter` button to restart.
 
 ## Windows-like UI
 
-### Fonts
-1. Open the following fonts and click on `Install`.
-    * [fonts/SFUIDisplay-Regular.otf](./fonts/SFUIDisplay-Regular.otf)
-    * [fonts/FiraCodeNerdFont-Retina.ttf](./fonts/FiraCodeNerdFont-Retina.ttf)
-
-1. Run the following commands:
-    ```sh
-    sudo apt update
-    sudo apt install gnome-tweaks -y
-    gnome-tweaks
-    ```
-
-1. Click on `Fonts`.
-
-1. Configure the following:
-    * Interface Text: `SF UI Display Normal` Size `11`
-    * Document Text: `SF UI Display Normal` Size `11`
-    * Monospace Text: `FiraCode Nerd Font Retina` Size `12`
-    * Legacy Window Titles: `SF UI Display Normal` Size `10`
-
 ### Extensions
 
-1.  Run the following commands:
+1. Open the Terminal.
+
+1. Run the following commands:
     ```sh
     sudo apt update
     sudo apt install gnome-shell-extension-manager -y
     extension-manager
     ```
 
-1. Click on `Browse` and `Install` the following extensions.
-    *  ArcMenu (Tip use search keyword `ArcMenu` and sort by `Downloads`)
-    *  Dash to Panel (Tip use search keyword `Panel` and sort by `Downloads`)
+1. Click on `Browse` and `Install` the following extensions as illustrated below:
+<a href="screenshots/extensions.png"><p align="center"><img src="screenshots/extensions.png" /></p></a>
+    *  **ArcMenu** (Tip: use search keyword `ArcMenu` and sort by `Downloads`)
+    *  **Dash to Panel** (Tip: use search keyword `Panel` and sort by `Downloads`)
+    *  **Sound Input & Output Device Chooser** (Tip: use search keyword `Sound` and sort by `Downloads`)
 
-### ArcMenu
 
-1. Go to the `Installed` tab and click on ⚙️ to configure `ArcMenu`.
+#### ArcMenu
+
+This extension is used to setup the Start menu similar to Windows.  Follow the instructions below to configure:
+
+<a href="screenshots/arcmenu.png"><p align="center"><img src="screenshots/arcmenu.png" /></p></a>
+
+1. Go to the `Installed` tab.
+
+1. Click on ⚙️ to configure `ArcMenu`.
 
 1. Switch to `About` tab.
 
-1. Click on `Load` and choose [extensions/ArcMenu](./extensions/ArcMenu) configuration file.
+1. Click on `Load` and choose [extensions/ArcMenu](./extensions/ArcMenu) configuration file from extracted folder.
 
-### Dash to Panel
+#### Dash to Panel
 
-1. Go to the `Installed` tab and click on ⚙️ to configure `Dash to Panel`.
+This extension is used to setup the Taskbar similar to Windows.  Follow the instructions below to configure:
+
+<a href="screenshots/dashtopanel.png"><p align="center"><img src="screenshots/dashtopanel.png" /></p></a>
+
+1. Go to the `Installed` tab.
+
+1. Click on ⚙️ to configure `Dash to Panel`.
 
 1. Switch to `About` tab.
 
-1. Click on `Import from file` and choose [extensions/DashtoPanel](./extensions/DashtoPanel) configuration file.
+1. Click on `Import from file` and choose [extensions/DashToPanel](./extensions/DashToPanel) configuration file from extracted folder.
+
+### Powerlevel10k configuration (optional)
+
+1. Open the Terminal.
+
+1. If you want to reconfigure Powerlevel10k, use the following command:
+    ```sh
+    p10k configure
+    ``` 
 
---
+++ script.sh
@@ -0,0 +1,239 @@
+#!/bin/bash
+
+CHOICES=( "$@" )
+
+# shellcheck source=/dev/null
+source /etc/os-release
+
+# Uncomment the below line to define a custom Ubuntu version
+#UBUNTU_CODENAME=jammy
+RIDER_FILENAME=JetBrains.Rider-2023.3-EAP3-233.9802.20.Checked.tar.gz
+RIDER_ROOT_FOLDER="JetBrains Rider-233.9802.20"
+
+# .NET pre-release version
+DOTNET_FILENAME=dotnet-sdk-8.0.100-rc.2.23502.2-linux-x64.tar.gz
+DOTNET_DOWNLOAD_LINK=https://download.visualstudio.microsoft.com/download/pr/9144f37e-b370-41ee-a86f-2d2a69251652/bc1d544112ec134184a5aec7f7a1eaf9/$DOTNET_FILENAME
+
+# --------------------------------------------------------------------------------
+
+COLOR_CYAN="\033[0;96m"
+COLOR_YELLOW="\033[0;93m"
+COLOR_RESET="\033[0m"
+
+tput reset
+
+echo -e "The following is the configuration that will be used during the installation:
+
+${COLOR_YELLOW}UBUNTU_CODENAME${COLOR_RESET} is ${COLOR_CYAN}$UBUNTU_CODENAME${COLOR_RESET}"
+
+if [[ ${CHOICES[*]} =~ '"5"' ]]; then
+  echo -e "${COLOR_YELLOW}DOTNET_FILENAME${COLOR_RESET} is ${COLOR_CYAN}$DOTNET_FILENAME${COLOR_RESET}
+${COLOR_YELLOW}DOTNET_DOWNLOAD_LINK${COLOR_RESET} is ${COLOR_CYAN}$DOTNET_DOWNLOAD_LINK${COLOR_RESET}"
+fi
+
+if [[ ${CHOICES[*]} =~ '"6"' ]]; then
+  echo -e "${COLOR_YELLOW}RIDER_FILENAME${COLOR_RESET} is ${COLOR_CYAN}$RIDER_FILENAME${COLOR_RESET}
+${COLOR_YELLOW}RIDER_ROOT_FOLDER${COLOR_RESET} is ${COLOR_CYAN}$RIDER_ROOT_FOLDER${COLOR_RESET}"
+fi
+
+echo -e "
+To change press ${COLOR_YELLOW}CTRL+C${COLOR_RESET} to terminate and hard-code variable in script.sh file.
+
+${COLOR_YELLOW}Press any key to continue.${COLOR_RESET}"
+
+read -r
+
+tput reset
+
+# --------------------------------------------------------------------------------
+
+echo Updating System
+sudo apt update
+sudo apt upgrade -y
+sudo snap refresh 
+
+# --------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"1"' ]]; then
+  echo Installing essential tools
+  sudo apt install curl git gitg htop tilix neovim -y
+fi
+
+# --------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"2"' ]]; then
+  echo Customising zsh
+  sudo apt install zsh git curl fzf -y
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/g' ~/.zshrc
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+
+  sed -i 's/^plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fzf-tab)/g' ~/.zshrc
+
+  sudo chsh -s "$(which zsh)" "$USER"
+
+  # shellcheck disable=SC2016
+  echo -e '
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+' >> ~/.zshrc 
+  cp ./.p10k.zsh ~/.p10k.zsh
+fi
+
+# --------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"3"' ]]; then
+  echo Installing Brew
+  sudo apt install git gcc curl -y
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/"$USER"/.zshrc
+  (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/"$USER"/.bashrc
+fi
+
+# --------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"4"' ]]; then
+  echo Installing Docker
+  sudo apt install ca-certificates curl gnupg -y
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo rm /etc/apt/keyrings/docker.gpg -f
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+  echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $UBUNTU_CODENAME stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+  sudo usermod -aG docker $USER
+fi
+
+# --------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"5"' ]]; then
+  echo Installing .NET
+  sudo apt-get install dotnet-sdk-7.0 -y
+
+  # Temporary installation for .NET pre-release version
+  wget $DOTNET_DOWNLOAD_LINK
+  sudo tar -xvzf $DOTNET_FILENAME --directory /usr/lib/dotnet
+  rm $DOTNET_FILENAME
+fi
+
+# --------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"6"' ]]; then
+  echo Installing Rider
+  wget https://download-cdn.jetbrains.com/rider/$RIDER_FILENAME
+  tar -xvzf $RIDER_FILENAME
+  mv "./$RIDER_ROOT_FOLDER/" rider
+  sudo mv ./rider /usr/lib/
+  rm $RIDER_FILENAME
+
+  echo Adding Rider shortcut
+  cat << EOF > ~/.local/share/applications/com.jetbrains-rider.desktop
+  [Desktop Entry]
+  Name=Rider
+  Exec=/usr/lib/rider/bin/rider.sh
+  Comment=JetBrains Rider IDE.
+  Terminal=false
+  PrefersNonDefaultGPU=false
+  Icon=/usr/lib/rider/bin/rider.png
+  Type=Application
+  Categories=Development;
+EOF
+  xdg-desktop-menu install ~/.local/share/applications/com.jetbrains-rider.desktop
+fi
+
+#--------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"7"' ]]; then
+  echo Installing Visual Studio Code
+  sudo apt install curl -y
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+  sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft-archive-keyring.gpg
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/usr/share/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo apt update
+  sudo apt-get install code -y
+  rm microsoft.gpg
+fi
+
+#--------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"8"' ]]; then
+  echo  Installing Google Chrome
+  wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo apt install ./google-chrome-stable_current_amd64.deb -y
+  rm google-chrome-stable_current_amd64.deb
+  sudo update-alternatives --set x-www-browser /usr/bin/google-chrome-stable
+fi
+
+#--------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"9"' ]]; then
+  echo Adding Customisations
+  cp -r ./fonts/ ~/.local/share/
+
+  # Bash
+  echo 'alias ..="cd .."' >> ~/.bashrc
+  echo 'alias ...="cd ../.."' >> ~/.bashrc
+  echo 'alias ....="cd ../../.."' >> ~/.bashrc
+  echo 'alias .....="cd ../../../.."' >> ~/.bashrc
+  echo 'alias g="git"' >> ~/.bashrc
+  echo 'alias ll="ls -hal --color=auto"' >> ~/.bashrc
+
+  # ZSH
+  echo 'alias ..="cd .."' >> ~/.zshrc
+  echo 'alias ...="cd ../.."' >> ~/.zshrc
+  echo 'alias ....="cd ../../.."' >> ~/.zshrc
+  echo 'alias .....="cd ../../../.."' >> ~/.zshrc
+  echo 'alias g="git"' >> ~/.zshrc
+  echo 'alias ll="ls -hal --color=auto"' >> ~/.zshrc
+
+  git config --global alias.co checkout
+  git config --global alias.ct commit
+  git config --global alias.st status
+  git config --global alias.br branch
+  git config --global alias.p push
+
+  # Cinnamon
+  gsettings set org.cinnamon.desktop.keybindings custom-list "['custom0']"
+  gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ binding "['<Primary>F12']"
+  gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ command "'tilix --quake --full-screen'"
+  gsettings set org.cinnamon.desktop.keybindings.custom-keybinding:/org/cinnamon/desktop/keybindings/custom-keybindings/custom0/ name "'Tilix'"
+
+  # GNOME
+  gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding "'<Primary>F12'"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command "'env GDK_BACKEND=x11 tilix --quake --full-screen'"
+  gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name "'Tilix'"
+
+  gsettings set org.gnome.desktop.interface font-name 'SF UI Display 11'
+  gsettings set org.gnome.desktop.interface document-font-name 'SF UI Display 11'
+  gsettings set org.gnome.desktop.interface monospace-font-name 'FiraCode Nerd Font weight=453 12'
+  gsettings set org.gnome.gedit.preferences.editor editor-font 'FiraCode Nerd Font Retina 12'
+  gsettings set org.gnome.desktop.wm.preferences titlebar-font 'SF UI Display 11'
+
+  sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper
+fi
+
+#--------------------------------------------------------------------------------
+
+if [[ ${CHOICES[*]} =~ '"10"' ]]; then
+  echo Configure Fingerprint Authentication
+  # Enable the following commands if desktop environment does not provide a way to configure fingerprints
+  #sudo apt install libpam-fprintd -y
+  #sudo fprintd-enroll -f right-index-finger
+  sudo pam-auth-update --enable fprintd
+fi
+
+#--------------------------------------------------------------------------------
+
+echo Cleanup
+sudo apt autoremove -y
